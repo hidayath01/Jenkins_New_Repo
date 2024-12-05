8bab1273d3e28e9b54d7f7c2a2b44846014d757e
Hedayath <hedayath.abdul@qapitol.com>
2024-12-05 10:05:53 +0530

    This is second commit

--- src/test/java/Class1.java
+++ src/test/java/Class1.java
@@ -1,4 +1,11 @@
+import org.testng.annotations.Test;
 
 public class Class1 {
+	
+	@Test
+	public void class1Method()
+	{
+		System.out.println("Class 1 Method");
+	}
 
 }
--- src/test/java/Class2.java
+++ src/test/java/Class2.java
@@ -1,4 +1,11 @@
+import org.testng.annotations.Test;
 
 public class Class2 {
+	
+	@Test
+	public void class2Method()
+	{
+		System.out.println("CLass two Method");
+	}
 
 }
